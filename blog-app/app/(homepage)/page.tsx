e24dd3f14d1b7ff2a8ac6dc6ccf2bcc5a0165278
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2024-08-16 14:33:52 +0300

    Styling home page

--- blog-app/app/(homepage)/page.tsx
+++ blog-app/app/(homepage)/page.tsx
@@ -1,7 +1,30 @@
-
+import Link from "next/link";
+import React from "react";
 
 export default function Home() {
   return (
-   <div className=" w-full h-screen bg-[url('../../images/cover.jpg')] bg-center bg-cover mitko"></div>
+    <div className=" w-full h-screen bg-[url('../../images/cover.jpg')] bg-bottom bg-cover bg-no-repeat flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center mt-64 gap-10">
+        <p className="font-extrabold text-[40px] text-white max-w-[350px]">Empower Your Voice. Illuminate minds.</p>
+        <Link
+          href={"/dashboard"}
+          className="bg-[#ffffd7] px-8 py-2 font-bold text-xl tracking-widest"
+        >
+          Login
+        </Link>
+        <Link
+          href={"/dashboard"}
+          className="bg-[#ffffd7] px-8 py-2 font-bold text-xl tracking-widest"
+        >
+          Register
+        </Link>
+        <Link
+          href={"/dashboard"}
+          className="bg-[#ffffd7] px-8 py-2 font-bold text-xl tracking-widest"
+        >
+          Start to Read
+        </Link>
+      </div>
+    </div>
   );
 }
