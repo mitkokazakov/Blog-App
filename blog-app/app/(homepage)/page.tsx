09cde68749fe3c7cc6d7baad6908c5f72c408c07
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2024-08-19 12:59:19 +0300

    Styling home page for mobile screens

--- blog-app/app/(homepage)/page.tsx
+++ blog-app/app/(homepage)/page.tsx
@@ -4,26 +4,34 @@ import React from "react";
 export default function Home() {
   return (
     <div className=" w-full h-screen bg-[url('../../images/cover.jpg')] bg-bottom bg-cover bg-no-repeat flex justify-center items-center">
-      <div className="flex flex-col justify-center items-center mt-64 gap-10">
-        <p className="font-extrabold text-[40px] text-white max-w-[350px]">Empower Your Voice. Illuminate minds.</p>
-        <Link
-          href={"/dashboard"}
-          className="bg-[#ffffd7] px-8 py-2 font-bold text-xl tracking-widest"
-        >
-          Login
-        </Link>
-        <Link
-          href={"/dashboard"}
-          className="bg-[#ffffd7] px-8 py-2 font-bold text-xl tracking-widest"
-        >
-          Register
-        </Link>
-        <Link
-          href={"/dashboard"}
-          className="bg-[#ffffd7] px-8 py-2 font-bold text-xl tracking-widest"
-        >
-          Start to Read
-        </Link>
+      <div className="h-full w-full py-10 flex flex-col justify-between items-center">
+        <div className="w-full px-6">
+          <p className="font-extrabold text-[40px] text-white max-w-[350px]">
+            Empower Your Voice. Illuminate minds.
+          </p>
+        </div>
+
+        <div className="w-full px-10 flex flex-col justify-center items-center  gap-10">
+          <Link
+            href={"/dashboard"}
+            className="w-full bg-[#ffffd7] px-8 py-2 font-bold text-xl tracking-widest text-center"
+          >
+            Login
+          </Link>
+          <Link
+            href={"/dashboard"}
+            className="w-full bg-[#ffffd7] px-8 py-2 font-bold text-xl tracking-widest text-center"
+          >
+            Register
+          </Link>
+          <p className="font-bold text-xl text-white">OR</p>
+          <Link
+            href={"/dashboard"}
+            className="w-full bg-[#ffffd7] px-8 py-2 font-bold text-xl tracking-widest text-center"
+          >
+            Start to Read
+          </Link>
+        </div>
       </div>
     </div>
   );
