19ea21c5f73e7b21e0f1c9805e232a8cf3b02c21
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2024-08-19 15:22:56 +0300

    Customize the home page for medium and large screens

--- blog-app/app/(homepage)/page.tsx
+++ blog-app/app/(homepage)/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className=" w-full h-screen bg-[url('../../images/cover.jpg')] bg-bottom bg-cover bg-no-repeat flex justify-center items-center">
       <div className="h-full w-full py-10 flex flex-col justify-between items-center">
-        <div className="w-full px-6">
-          <p className="font-extrabold text-[40px] text-white max-w-[350px]">
+        <div className="w-full px-6 md:px-10">
+          <p className="font-extrabold text-[36px] max-w-[350px] text-white md:text-[40px]">
             Empower Your Voice. Illuminate minds.
           </p>
         </div>
@@ -14,20 +14,20 @@ export default function Home() {
         <div className="w-full px-10 flex flex-col justify-center items-center  gap-10">
           <Link
             href={"/dashboard"}
-            className="w-full bg-[#ffffd7] px-8 py-2 font-bold text-xl tracking-widest text-center"
+            className="w-full bg-[#ffffd7] px-8 py-2 font-medium text-xl tracking-widest text-center sm:w-64"
           >
             Login
           </Link>
           <Link
             href={"/dashboard"}
-            className="w-full bg-[#ffffd7] px-8 py-2 font-bold text-xl tracking-widest text-center"
+            className="w-full bg-[#ffffd7] px-8 py-2 font-medium text-xl tracking-widest text-center sm:w-64"
           >
             Register
           </Link>
           <p className="font-bold text-xl text-white">OR</p>
           <Link
             href={"/dashboard"}
-            className="w-full bg-[#ffffd7] px-8 py-2 font-bold text-xl tracking-widest text-center"
+            className="w-full bg-[#ffffd7] px-8 py-2 font-medium text-xl tracking-widest text-center sm:w-64"
           >
             Start to Read
           </Link>
