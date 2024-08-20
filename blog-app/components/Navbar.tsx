78b91d697634fffa8593aff7e90010bf0e48855b
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2024-08-20 13:06:15 +0300

    Implement logic for dropdown menu

--- blog-app/components/Navbar.tsx
+++ blog-app/components/Navbar.tsx
@@ -7,10 +7,6 @@ const Navbar = () => {
 
   const [dropdownActive, setDropdown] = useState(false);
 
-  function ClickProfile(){
-
-    
-  }
 
   return (
     <div className='relative w-full h-20 bg-[#ffffd7] border-b-[1px] border-b-black flex justify-between items-center px-5'>
@@ -21,11 +17,11 @@ const Navbar = () => {
 
       <div className='flex justify-center items-center gap-5'>
         {/* <Link href={"/"}>Login</Link> */}
-        < PiSignIn className='text-[36px]' />
+        < PiSignIn className='text-[30px]' />
         <div onClick={() => {setDropdown(!dropdownActive)}} className='w-16 h-16 rounded-full bg-white flex justify-center items-center text-2xl'>M</div>
       </div>
 
-      <div  className={dropdownActive ? 'absolute right-3 top-[101%] w-[150px] min-h-28 bg-white flex flex-col gap-5 justify-center items-start px-2 py-2 duration-300' : 'absolute w-[150px] х-2 opacity-0 top-0 right-3 pointer-events-none duration-300'}>
+      <div  className={`absolute overflow-hidden right-3 top-[101%] w-[150px] bg-white flex flex-col gap-5 justify-center items-start px-2 py-2 duration-500 ${dropdownActive ? 'h-32 opacity-1 pointer-events-auto' : 'h-0 opacity-0 pointer-events-none'}`}>
           <Link href={"/"}>My Blogs</Link>
           <Link href={"/"}>Create Blog</Link>
           <Link href={"/"}>Log Out</Link>
