fc13bbd2badac9e5c5374e25c0ec4e534bb2fea5
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2024-08-26 12:53:46 +0300

    Working on froala editor for uploading images

--- blog-app/app/(client)/create/page.tsx
+++ blog-app/app/(client)/create/page.tsx
@@ -1,21 +1,31 @@
-"use client"
-import React from 'react'
-import '../../../node_modules/froala-editor/css/froala_style.min.css'
-import Tiptap from '@/components/Tiptap'
-import FroalaEditor from 'react-froala-wysiwyg'
-import '../../../node_modules/froala-editor/css/froala_style.css'
-import '../../../node_modules/froala-editor/css/froala_editor.css'
+"use client";
+import React, { useState } from "react";
+import "../../../node_modules/froala-editor/css/froala_style.min.css";
+import Tiptap from "@/components/Tiptap";
+import FroalaEditor from "react-froala-wysiwyg";
+import "../../../node_modules/froala-editor/css/froala_style.css";
+import "../../../node_modules/froala-editor/css/froala_editor.css";
+import "../../../node_modules/froala-editor/js/plugins/image.min.js";
+import "../../../node_modules/froala-editor/js/plugins/image_manager.min.js";
+import "../../../node_modules/froala-editor/js/plugins/align.min.js";
+import "../../../node_modules/froala-editor/js/plugins/paragraph_format.min.js";
+import "../../../node_modules/froala-editor/js/plugins/paragraph_style.min.js";
+import "../../../node_modules/froala-editor/js/plugins/align.min.js";
+import "../../../node_modules/froala-editor/js/plugins/lists.min.js";
+import "../../../node_modules/froala-editor/js/plugins/save.min.js";
+import "../../../node_modules/froala-editor/js/froala_editor.pkgd.min.js";
+import "../../../node_modules/froala-editor/js/froala_editor.min.js";
 
 const page = () => {
+  const [blog, setBlog] = useState(() => {
+    let fromLocaleStorage = localStorage.getItem("blog") || "";
 
-    
-      
+    return fromLocaleStorage;
+  });
+
   return (
     <div className="mt-10 sm:mx-auto sm:w-full md:w-[800px]">
-      <form
-        method="POST"
-        className="space-y-6"
-      >
+      <form method="POST" className="space-y-6">
         <div>
           <label
             htmlFor="blogtitle"
@@ -29,11 +39,8 @@ const page = () => {
               type="text"
               required
               className="block w-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              
             />
-            <span className="text-red-600 tracking-widest text-sm">
-             
-            </span>
+            <span className="text-red-600 tracking-widest text-sm"></span>
           </div>
         </div>
 
@@ -50,11 +57,8 @@ const page = () => {
               required
               className="block w-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               rows={7}
-              
             />
-            <span className="text-red-600 tracking-widest text-sm">
-              
-            </span>
+            <span className="text-red-600 tracking-widest text-sm"></span>
           </div>
         </div>
 
@@ -67,10 +71,48 @@ const page = () => {
           </label>
           <div className="mt-2">
             {/* <Tiptap /> */}
-            <FroalaEditor />
-            <span className="text-red-600 tracking-widest text-sm">
-              
-            </span>
+            <FroalaEditor
+              config={{
+                placeholderText: "Insert here",
+                imageAllowedTypes: ["jpeg", "jpg", "png"],
+                saveInterval: 1000,
+                toolbarButtons: {
+                  moreText: {
+                    buttons: [
+                      "italic",
+                      "bold",
+                      "underline",
+                      "strikeThrough",
+                      "subscript",
+                      "superscript",
+                    ],
+                  },
+                  moreParagraph: {
+                    buttons: ["alignLeft", "alignRight"],
+                  },
+                  moreRich: {
+                    buttons: ["insertHR", "insertImage"],
+                  },
+                },
+                events: {
+                  "save.before": function (html: string) {
+                    localStorage.setItem("blog", html);
+                  },
+                  "image.inserted": function($img: any){
+                    const src: string = $img.attr("src");
+                    const modifyiedSrc = src.slice(5);
+                    fetch(modifyiedSrc).then(res => res.blob()).then(blob =>{console.log(blob);
+                    })
+                    
+                  }
+                },
+              }}
+              model={blog}
+              onModelChange={(e: string) => setBlog(e)}
+              tag="textarea"
+            />
+
+            <span className="text-red-600 tracking-widest text-sm"></span>
           </div>
         </div>
 
@@ -90,16 +132,15 @@ const page = () => {
               required
               autoComplete="current-password"
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              
             />
-            <span className="text-red-600 tracking-widest text-sm">
-              
-            </span>
+            <span className="text-red-600 tracking-widest text-sm"></span>
           </div>
         </div>
 
         <div>
-        <button
+          <button
+          onClick={() => {console.log(blog);
+          }}
             type="submit"
             className="flex w-full justify-center rounded-md bg-[#ffffd7] px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-[2px_2px_8px_0px_rgba(0,0,0,0.3)]  hover:bg-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
           >
@@ -108,7 +149,7 @@ const page = () => {
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
