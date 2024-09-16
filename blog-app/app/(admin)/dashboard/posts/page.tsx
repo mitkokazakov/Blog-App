19fbc9d7dff5920622e93c50755fae776b56e719
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2024-09-16 13:43:55 +0300

    Implement filtering in admin posts page

--- blog-app/app/(admin)/dashboard/posts/page.tsx
+++ blog-app/app/(admin)/dashboard/posts/page.tsx
@@ -2,14 +2,20 @@ import DashboardAllPosts from '@/components/AdminComponents/DashboardAllPosts'
 import Link from 'next/link'
 import React from 'react'
 
-const page = () => {
+type AllPostsParams = {
+    searchParams: {
+        tab: string
+    }
+}
+
+const page = ( {searchParams}: AllPostsParams) => {
   return (
     <div className="w-full bg-[#0f0f11] h-screen text-white flex flex-col gap-8 px-10 py-10">
       <div>
         <h1 className="font-bold tracking-widest text-2xl">All Posts</h1>
       </div>
 
-      <DashboardAllPosts />
+      <DashboardAllPosts tab={searchParams.tab}/>
 
       {/* <div className="flex justify-between items-center">
           <input
--- blog-app/components/AdminComponents/DashboardAllPosts.tsx
+++ blog-app/components/AdminComponents/DashboardAllPosts.tsx
@@ -1,118 +1,192 @@
-"use client"
-import Link from 'next/link'
-import React, { useState } from 'react'
+"use client";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
-const data = [
-    {
-        id: "1a",
-        title: "Best places to see the northern lights",
-        description: "The best places to see the northern light is probably Norway, Finland, Iceland and Canada.",
-        createdAt: "Nov 04 2024",
-        status: "approved"
-    },
-    {
-        id: "2a",
-        title: "Tromso - the northernmost city",
-        description: "The best places to see the northern light is probably Norway, Finland, Iceland and Canada.",
-        createdAt: "Nov 04 2024",
-        status: "not approved"
-    },
-    {
-        id: "3a",
-        title: "Big snowstorm is comming?",
-        description: "The best places to see the northern light is probably Norway, Finland, Iceland and Canada.",
-        createdAt: "Nov 04 2024",
-        status: "approved"
-    },
-    {
-        id: "4a",
-        title: "Which country is best for living",
-        description: "The best places to see the northern light is probably Norway, Finland, Iceland and Canada.",
-        createdAt: "Nov 06 2024",
-        status: "not approved"
-    }
+type PostType = {
+  id: string;
+  title: string;
+  description: string;
+  createdAt: string;
+  status: string;
+};
+
+const data: PostType[] = [
+  {
+    id: "1a",
+    title: "Best places to see the northern lights",
+    description:
+      "The best places to see the northern light is probably Norway, Finland, Iceland and Canada.",
+    createdAt: "Nov 04 2024",
+    status: "approved",
+  },
+  {
+    id: "2a",
+    title: "Tromso - the northernmost city",
+    description:
+      "The best places to see the northern light is probably Norway, Finland, Iceland and Canada.",
+    createdAt: "Nov 04 2024",
+    status: "not approved",
+  },
+  {
+    id: "3a",
+    title: "Big snowstorm is comming?",
+    description:
+      "The best places to see the northern light is probably Norway, Finland, Iceland and Canada.",
+    createdAt: "Nov 04 2024",
+    status: "approved",
+  },
+  {
+    id: "4a",
+    title: "Which country is best for living",
+    description:
+      "The best places to see the northern light is probably Norway, Finland, Iceland and Canada.",
+    createdAt: "Nov 06 2024",
+    status: "not approved",
+  },
 ];
 
-const DashboardAllPosts = () => {
+const DashboardAllPosts = ({ tab }: { tab: string }) => {
+  const [inputSearch, setInputSearch] = useState("");
+  const [filteredData, SetFilteredData] = useState<PostType[]>(data);
 
-    const [inputSearch, setInputSearch] = useState("");
+  //let filteredData = data;
+
+  useEffect(() => {
+    if (tab == "approved") {
+        //filteredData = data.filter((p) => p.status == "approved");
+
+        const tempData = data.filter(p => p.status == "approved")
+
+        SetFilteredData(tempData)
+      }
+      if (tab == "not approved") {
+        //filteredData = data.filter((p) => p.status == "not approved");
+
+        const tempData = data.filter(p => p.status == "not approved")
+
+        SetFilteredData(tempData)
+      }
+      if (tab == "all") {
+        //filteredData = data;
+        SetFilteredData(data)
+      }
+  },[tab])
+
+//   if (tab == "approved") {
+//     filteredData = data.filter((p) => p.status == "approved");
+//   }
+//   if (tab == "not approved") {
+//     filteredData = data.filter((p) => p.status == "not approved");
+//   }
+//   if (tab == "all") {
+//     filteredData = data;
+//   }
+
+//   function OnChangeInput(data: PostType[]){
+
+//   }
 
   return (
     <div>
-    <div className="flex justify-between items-center mb-5">
-          <input
-            type="text"
-            name="username"
-            id="username"
-            className="px-4 py-1 bg-slate-800 rounded-lg"
-            placeholder="Search for a post"
-          />
+      <div className="flex justify-between items-center mb-5">
+        <input
+          type="text"
+          name="username"
+          id="username"
+          className="px-4 py-1 bg-slate-800 rounded-lg"
+          placeholder="Search for a post"
+          onChange={(e) => setInputSearch(e.target.value)}
+        />
 
-          <div className='flex justify-center items-center gap-5'>
-            <Link href={'/'} className='bg-yellow-600 px-3 py-2 rounded-lg tracking-widest'>Approved</Link>
-            <Link href={'/'} className='bg-yellow-600 px-3 py-2 rounded-lg tracking-widest'>Not approved</Link>
-            <Link href={'/'} className='bg-yellow-600 px-3 py-2 rounded-lg tracking-widest'>All</Link>
-          </div>
-
-          <div>
-          <Link href={"/"} className="bg-purple-600 px-3 py-2 rounded-lg">
-            Add Post
+        <div className="flex justify-center items-center gap-5">
+          <Link
+            href={"/dashboard/posts?tab=approved"}
+            className="bg-yellow-600 px-3 py-2 rounded-lg tracking-widest"
+          >
+            Approved
           </Link>
-          </div>
+          <Link
+            href={"/dashboard/posts?tab=not approved"}
+            className="bg-yellow-600 px-3 py-2 rounded-lg tracking-widest"
+          >
+            Not approved
+          </Link>
+          <Link
+            href={"/dashboard/posts?tab=all"}
+            className="bg-yellow-600 px-3 py-2 rounded-lg tracking-widest"
+          >
+            All
+          </Link>
         </div>
 
         <div>
-        <table className="table-auto border-collapse border border-slate-400 w-full">
-            <thead>
-              <tr>
-                <th className="text-left border border-slate-800 px-4 py-4">
-                  Blog Title
-                </th>
-                <th className="text-left border border-slate-800 px-4 py-4">
-                  Short Description
-                </th>
-                <th className="text-left border border-slate-800 px-4 py-4">
-                  Created at
-                </th>
-                <th className="text-left border border-slate-800 px-4 py-4">
-                  Status
-                </th>
-                <th className="text-left border border-slate-800 px-4 py-4">
-                  Action
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="text-left border border-slate-800 px-4 py-4 ">
-                  <p className=' line-clamp-1'>Best places to see the northern lights</p>
-                </td>
-                <td className="text-left border border-slate-800 px-4 py-4 ">
-                  <p className=' line-clamp-1 max-w-[300px]'>The best places to see the northern light is probably Norway, Finland, Iceland and Canada.</p> 
-                </td>
-                <td className="text-left border border-slate-800 px-4 py-4">
-                  Nov 04 2024
-                </td>
-                <td className="text-left border border-slate-800 px-4 py-4">
-                  approved
-                </td>
-                <td className="text-left border border-slate-800 px-4 py-4 ">
-                  <Link
-                    href={"/dashboard/users/33"}
-                    className="bg-green-500 px-3 py-1 rounded-lg mr-5"
-                  >
-                    View
-                  </Link>
-                  <Link href={"/"} className="bg-red-500 px-3 py-1 rounded-lg">
-                    Delete
-                  </Link>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <Link href={"/"} className="bg-purple-600 px-3 py-2 rounded-lg">
+            Add Post
+          </Link>
         </div>
-        </div>
-  )
-}
+      </div>
 
-export default DashboardAllPosts
+      <div>
+        <table className="table-auto border-collapse border border-slate-400 w-full">
+          <thead>
+            <tr>
+              <th className="text-left border border-slate-800 px-4 py-4">
+                Blog Title
+              </th>
+              <th className="text-left border border-slate-800 px-4 py-4">
+                Short Description
+              </th>
+              <th className="text-left border border-slate-800 px-4 py-4">
+                Created at
+              </th>
+              <th className="text-left border border-slate-800 px-4 py-4">
+                Status
+              </th>
+              <th className="text-left border border-slate-800 px-4 py-4">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData?.map((p) => {
+              return (
+                <tr key={p.id}>
+                  <td className="text-left border border-slate-800 px-4 py-4 ">
+                    <p className=" line-clamp-1">{p.title}</p>
+                  </td>
+                  <td className="text-left border border-slate-800 px-4 py-4 ">
+                    <p className=" line-clamp-1 max-w-[300px]">
+                      {p.description}
+                    </p>
+                  </td>
+                  <td className="text-left border border-slate-800 px-4 py-4">
+                    {p.createdAt}
+                  </td>
+                  <td className="text-left border border-slate-800 px-4 py-4">
+                    {p.status}
+                  </td>
+                  <td className="text-left border border-slate-800 px-4 py-4 ">
+                    <Link
+                      href={"/dashboard/users/33"}
+                      className="bg-green-500 px-3 py-1 rounded-lg mr-5"
+                    >
+                      View
+                    </Link>
+                    <Link
+                      href={"/"}
+                      className="bg-red-500 px-3 py-1 rounded-lg"
+                    >
+                      Delete
+                    </Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardAllPosts;
